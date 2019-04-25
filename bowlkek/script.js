53fa5f3ced8ffa64f3226fee9e58f6be2897b584
Brandon Lee <brandononlyu@gmail.com>
2019-04-25 17:14:38 -0400

    Fixed incorrect split in list separation

--- bowlkek/script.js
+++ bowlkek/script.js
@@ -1,6 +1,6 @@
 const url = 'https://raw.githubusercontent.com/CurtisGrayeBabin/List-of-all-Foods/master/FOOD.txt'
 $(document).ready(function(){
 	$.get(url, function(data, status){
-		$("#content").text(data.split(" ")[Math.floor(Math.random() * 20000) + 1])
+		$("#content").text(data.split("\n")[Math.floor(Math.random() * 20000) + 1])
 	})
 })
